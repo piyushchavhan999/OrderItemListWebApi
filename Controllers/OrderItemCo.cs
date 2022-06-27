d9f86a1c00c6d502c00280316624d8ec0a33ebf4
piyush <piyushchavhan999@gmail.com>
2022-06-27 19:36:42 +0530

    1st commit

---
+++ Controllers/OrderItemCo.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OrderItemListWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OrderItemListWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromQuery] int id)
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("http://20.127.252.97/api/");
+                var responseTask = client.GetAsync("MenuItem");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                List<OrderItem> Items = new List<OrderItem>();
+
+                if (result.IsSuccessStatusCode)
+                {
+
+                    string jsonData = result.Content.ReadAsStringAsync().Result;
+
+
+                    Items = JsonConvert.DeserializeObject<List<OrderItem>>(jsonData);
+                    OrderItem c = Items.SingleOrDefault(item => item.Id == id);
+
+                    c.MenuItemId = 1;
+                    c.UserId = 1;
+
+
+                    return Ok(c);
+
+                }
+                else
+                {
+
+                    return BadRequest();
+
+                }
+
+            };
+        }
+
+
+    }
+
+}
---
+++ model/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderItemListWebApi.Models
+{
+    public class OrderItem
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public int MenuItemId { get; set; }
+
+
+        public string Name { get; set; }
+    }
+}
